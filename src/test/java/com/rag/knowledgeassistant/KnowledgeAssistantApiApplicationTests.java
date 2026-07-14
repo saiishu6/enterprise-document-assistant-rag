5474192632bc3017f794492dd9e7ead6d3244059
Iswariya Ananthanarayanan <saiishu.6@gmail.com>
2026-07-14 10:15:21 +0200

    Initial Spring Boot project setup

---
+++ src/test/java/com/rag/knowledgeassistant/KnowledgeAssistantApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rag.knowledgeassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeAssistantApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
